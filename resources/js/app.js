2f080b0312a9777ec1f7a21d30aaebbcba2f1f36
Ariel Gema Wardana <2024.arielgemaw.rplb@student.unsia.ac.id>
2025-02-03 21:07:53 +0700

    feat: create project

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import "./bootstrap";
+import "../assets/static/js/components/dark";
+import "../assets/static/js/pages/horizontal-layout";
+import "../assets/extensions/perfect-scrollbar/perfect-scrollbar.min";
+import "../assets/compiled/js/app";
+// import "../assets/extensions/apexcharts/apexcharts.min";
+// import "../assets/static/js/pages/dashboard";
