9e390fc15acb8da8a2629c0a463c91450438f072
Ariel Gema Wardana <2024.arielgemaw.rplb@student.unsia.ac.id>
2025-03-01 23:57:56 +0700

    feat(stocks): create filter column

--- resources/assets/static/js/pages/datatables.js
+++ resources/assets/static/js/pages/datatables.js
@@ -1,5 +1,36 @@
+$(document).ready(function () {
+    // Inisialisasi DataTables
+    var table = $("#table1").DataTable();
+
+    $("div.dataTables_filter", table.table().container()).css({
+        display: "flex",
+        "align-items": "center",
+        "justify-content": "flex-end",
+        gap: "10px",
+    });
+
+    // Event handler untuk checkbox
+    $('#toggle-columns input[type="checkbox"]').on("change", function () {
+        // Ambil indeks kolom dari atribut data-column
+        var column = table.column($(this).attr("data-column"));
+        // Toggle visibilitas kolom berdasarkan status checkbox
+        column.visible(this.checked);
+    });
+
+    // Sembunyikan kolom yang tidak dicentang saat inisialisasi
+    $('#toggle-columns input[type="checkbox"]').each(function () {
+        var column = table.column($(this).attr("data-column"));
+        column.visible(this.checked);
+    });
+
+    $("#dropdown-columns").appendTo(
+        $("div.dataTables_filter", table.table().container())
+    );
+});
+
 let jquery_datatable = $("#table1").DataTable({
     responsive: true,
+    scrollX: false,
     language: {
         search: "Cari: ",
         lengthMenu: "Tampilkan _MENU_ data",
@@ -14,9 +45,43 @@ let jquery_datatable = $("#table1").DataTable({
             next: "Berikutnya",
             previous: "Sebelumnya",
         },
-        columnDefs: [{ orderable: false, targets: [0, -1] }],
+        columnDefs: [
+            { width: "100px", targets: 0 },
+            { width: "100px", targets: 1 },
+            { width: "100px", targets: 2 },
+            { width: "100px", targets: 3 },
+            { width: "100px", targets: 4 },
+            { width: "100px", targets: 5 },
+            { width: "100px", targets: 6 },
+            { width: "100px", targets: 7 },
+            { width: "100px", targets: 8 },
+            { width: "100px", targets: 9 },
+            { width: "100px", targets: 10 },
+            { width: "100px", targets: 11 },
+            { width: "100px", targets: 12 },
+            { width: "100px", targets: 13 },
+            { width: "100px", targets: 14 },
+            { width: "100px", targets: 15 },
+            { width: "100px", targets: 16 },
+            { orderable: false, targets: [0, -1] },
+        ],
+        dom:
+            "<'row'<'col-3'l><'col-9 d-flex justify-content-end align-items-center'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-4'i><'col-8'p>>",
     },
 });
+
+const setTableColor = () => {
+    document
+        .querySelectorAll(".dataTables_paginate .pagination")
+        .forEach((dt) => {
+            dt.classList.add("pagination-danger");
+        });
+};
+setTableColor();
+jquery_datatable.on("draw", setTableColor);
+
 let customized_datatable = $("#table2").DataTable({
     responsive: true,
     pagingType: "simple",
@@ -31,13 +96,3 @@ let customized_datatable = $("#table2").DataTable({
         searchPlaceholder: "Search..",
     },
 });
-
-const setTableColor = () => {
-    document
-        .querySelectorAll(".dataTables_paginate .pagination")
-        .forEach((dt) => {
-            dt.classList.add("pagination-danger");
-        });
-};
-setTableColor();
-jquery_datatable.on("draw", setTableColor);
--- resources/views/components/pages/stocks/index.blade.php
+++ resources/views/components/pages/stocks/index.blade.php
@@ -14,25 +14,113 @@
         <div class="card">
             <div class="card-body">
                 <div class="table-responsive pt-2 pe-2">
+                    <div class="dropdown" id="dropdown-columns">
+                        <button class="btn btn-danger btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            <i class="bi bi-funnel"></i>
+                            <span class="visually-hidden">Filter</span>
+                        </button>
+                        <div class="dropdown-menu">
+                            <div id="toggle-columns" class="card p-3 mb-3">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" data-column="0" checked>
+                                    <label class="form-check-label">Foto</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" data-column="1" checked>
+                                    <label class="form-check-label">Kode Barang</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" data-column="2" checked>
+                                    <label class="form-check-label">Nama Barang</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" data-column="3">
+                                    <label class="form-check-label">Satuan</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" data-column="4" checked>
+                                    <label class="form-check-label">Kategori</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" data-column="5">
+                                    <label class="form-check-label">Grade</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" data-column="6">
+                                    <label class="form-check-label">IMEI 1</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" data-column="7">
+                                    <label class="form-check-label">IMEI 2</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" data-column="8" checked>
+                                    <label class="form-check-label">Jumlah Stok</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" data-column="9">
+                                    <label class="form-check-label">Modal</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" data-column="10" checked>
+                                    <label class="form-check-label">Harga Jual</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" data-column="11">
+                                    <label class="form-check-label">Invoice</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" data-column="12" checked>
+                                    <label class="form-check-label">Supplier</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" data-column="13">
+                                    <label class="form-check-label">No Kontak Supplier</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" data-column="14" checked>
+                                    <label class="form-check-label">Tanggal</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" data-column="15">
+                                    <label class="form-check-label">Keterangan</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <table class="table" id="table1">
                         <thead>
                             <tr>
                                 <th class="text-nowrap" data-orderable="false">Foto</th>
                                 <th class="text-nowrap">Kode Barang</th>
                                 <th class="text-nowrap">Nama Barang</th>
-                                {{-- <th class="text-nowrap">Satuan</th> --}}
+                                <th class="text-nowrap">Satuan</th>
                                 <th class="text-nowrap">Kategori</th>
-                                {{-- <th class="text-nowrap">Grade</th>
+                                <th class="text-nowrap">Grade</th>
                                 <th class="text-nowrap">IMEI 1</th>
-                                <th class="text-nowrap">IMEI 2</th> --}}
+                                <th class="text-nowrap">IMEI 2</th>
                                 <th class="text-nowrap">Jumlah Stok</th>
-                                {{-- <th class="text-nowrap">Modal</th> --}}
+                                <th class="text-nowrap">Modal</th>
                                 <th class="text-nowrap">Harga Jual</th>
-                                {{-- <th class="text-nowrap">Invoice</th> --}}
+                                <th class="text-nowrap">Invoice</th>
                                 <th class="text-nowrap">Supplier</th>
-                                {{-- <th class="text-nowrap">No Kontak Supplier</th> --}}
+                                <th class="text-nowrap">No Kontak Supplier</th>
                                 <th class="text-nowrap">Tanggal</th>
-                                {{-- <th class="text-nowrap">Keterangan</th> --}}
+                                <th class="text-nowrap">Keterangan</th>
                                 <th class="text-nowrap text-center" data-orderable="false"></th>
                             </tr>
                         </thead>
@@ -45,19 +133,19 @@
                                     </td>
                                     <td class="text-nowrap">{{ $stock->kode_barang }}</td>
                                     <td class="text-nowrap">{{ $stock->nama_barang }}</td>
-                                    {{-- <td class="text-nowrap">{{ $stock->satuan }}</td> --}}
+                                    <td class="text-nowrap">{{ $stock->satuan }}</td>
                                     <td class="text-nowrap">{{ $stock->kategori }}</td>
-                                    {{-- <td class="text-nowrap">{{ $stock->grade }}</td>
+                                    <td class="text-nowrap">{{ $stock->grade }}</td>
                                     <td class="text-nowrap">{{ $stock->imei_1 }}</td>
-                                    <td class="text-nowrap">{{ $stock->imei_2 }}</td> --}}
+                                    <td class="text-nowrap">{{ $stock->imei_2 }}</td>
                                     <td class="text-nowrap">{{ $stock->jumlah_stok }}</td>
-                                    {{-- <td class="text-nowrap">{{ $stock->modal }}</td> --}}
+                                    <td class="text-nowrap">{{ $stock->modal }}</td>
                                     <td class="text-nowrap">{{ $stock->harga_jual }}</td>
-                                    {{-- <td class="text-nowrap">{{ $stock->invoice }}</td> --}}
+                                    <td class="text-nowrap">{{ $stock->invoice }}</td>
                                     <td class="text-nowrap">{{ $stock->supplier }}</td>
-                                    {{-- <td class="text-nowrap">{{ $stock->no_kontak_supplier }}</td> --}}
+                                    <td class="text-nowrap">{{ $stock->no_kontak_supplier }}</td>
                                     <td class="text-nowrap">{{ $stock->tanggal }}</td>
-                                    {{-- <td class="text-nowrap">{{ $stock->keterangan }}</td> --}}
+                                    <td class="text-nowrap">{{ $stock->keterangan }}</td>
                                     <td class="d-inline-flex">
                                         {{-- dropdown but not button --}}
                                         <div class="dropdown">
